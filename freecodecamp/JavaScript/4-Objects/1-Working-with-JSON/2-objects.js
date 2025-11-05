a665b16574b1dd1d1c8c6865073aaaf8f43abe98
brarris <farisabrar62@gmail.com>
2025-11-05 17:43:43 +0700

    re organized, add repitition

--- freecodecamp/JavaScript/4-Objects/1-Working-with-JSON/2-objects.js
+++ freecodecamp/JavaScript/4-Objects/1-Working-with-JSON/2-objects.js
@@ -10,4 +10,31 @@ const developerObj = {
   country: "USA",
 };
 
-console.log(JSON.stringify(developerObj, null, 2));
+const jsonString = JSON.stringify(developerObj);
+console.log(jsonString); // {"firstName":"Jessica","isAwesome":true,"isMusician":true,"country":"USA"}
+/**
+ * In the example, the JavaScript object user is converted into a JSON string that looks like on the output above
+ */
+
+console.log(JSON.stringify(developerObj, ["firstName", "country"])); // {"firstName":"Jessica","country":"USA"}
+/**
+ * On the example above, we have a developerObj with four properties. When we use the JSON.stringify() method,
+ * we can pass in an array for the second parameter and specify which properties we want stringified.
+ * The result will be a stringified object containing only the firstName and country properties.
+ */
+
+/**
+ * Another method you will be using a lot in your programming is the JSON.parse() method. JSON.parse() converts a JSON string back into a JavaScript object.
+ * This is useful when you retrieve JSON data from a web server or from localStorage and you need to manipulate the data in your application. You will learn more about localStorage in a future lesson.
+ */
+const jsonstring = '{"name":"John","age":30,"isAdmin":true}';
+const userObject = JSON.parse(jsonstring);
+console.log(userObject);
+/**
+ * The JSON string is parsed back into a JavaScript object that looks like this
+ * {
+ * name: "John",
+ * age: 30,
+ * isAdmin: true
+ * }
+ */
